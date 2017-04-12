eacc9156065b0fa69e970ff467dc102899ce213f
JW Garrison <jwolfgarrison@gmail.com>
2017-04-11 20:34:21 -0400

    Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/,
+      },
+    ],
+  },
 };
